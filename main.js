71757065977d8286ed9d0354136605a62b236fcb
Vinyl-Davyl <okononfuadavid@gmail.com>
2022-05-04 08:45:26 -0700

    Initial commit
    City Tour Component

---
+++ main.js
@@ -0,0 +1,19 @@
+// const toggleTable = () => {
+//     const table = document.getElementById('');
+//     table.classList.toggle('table--full');
+//     table.classList.toggle('table--shrink');
+//   }
+  
+//   const toggleRow = (e) => {
+//     const tableRow = e.target.parentNode.parentNode;
+    
+//     tableRow.classList.toggle('shrunk');
+    
+//     toggleTable();
+//   }
+  
+//   const icons = document.getElementsByClassName('icon--small');
+  
+//   Array.from(icons).forEach(function(element) {
+//     element.addEventListener('click', toggleRow);
+//   });
